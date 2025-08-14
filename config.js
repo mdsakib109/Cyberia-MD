c241fd2ad43aa4421fa2970631a7341c19b7cbfc
mdsakib109 <mdsjjsjshshshs@gmail.com>
2025-08-14 12:11:40 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  SESSION_ID: "drakonis~5y9D2aaC#rb-qX_kfsHpLb8OuFxB1cSkjDvX1N0xft6Ih4oHkTJM",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "8801624906339", // PUT OWNER NUMBER HERE
+  PREFIX: "*" // prefix (e.g., ., /, !, *)
 };
